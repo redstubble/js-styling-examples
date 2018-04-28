979e2ffafe6c5571a85e697e0d2de0c32f9e5e7c
John Williams <john@redstubble.com>
2018-04-28 15:45:05 +1200

    Showing Flexbox Uses

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, AppRegistry } from 'react-native'
 
-export default class App extends React.Component {
+class FlexboxExamples extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <View style={styles.containerOne}>
+          <View style={styles.boxOne}/>
+          <View style={styles.boxOne}/>
+          <View style={styles.boxOne}/>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={[styles.containerTwo, {flexDirection: 'row'}]}>
+            <View style={styles.boxTwoOne}/>
+            <View style={styles.boxTwoOne}/>
+            <View style={styles.boxTwoOne}/>
+          </View>
+          <View style={[styles.containerTwo, {flexDirection: 'column',}]}>
+            <View style={styles.boxTwoTwo}/>
+            <View style={styles.boxTwoTwo}/>
+            <View style={styles.boxTwoTwo}/>
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOne: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around', // ''flex-start'' || center' || 'flex-end' || 'space-around' || 'space-between'
+    alignItems: "stretch", // flex-start center flex-end stretch(without specified width or height)
+    
   },
-});
+  boxOne: {
+    height: 50,
+    // width: 50,
+    backgroundColor: '#e76e63',
+    margin: 10,
+  },
+  containerTwo: {
+    flex: 1,
+   // flexDirection: 'column', // 'column' || 'row'
+   justifyContent: 'space-between', // ''flex-start'' || center' || 'flex-end' || 'space-around' || 'space-between'
+   alignItems: "stretch" // flex-start center flex-end stretch(without specified width or height)
+  },
+  boxTwoOne: {
+    //height: 50,
+    width: 50,
+    backgroundColor: '#e76e63',
+    margin: 10,
+  },
+  boxTwoTwo: {
+    height: 50,
+    //width: 50,
+    backgroundColor: '#e76e63',
+    margin: 10,
+  }
+})
+
+export default FlexboxExamples;
