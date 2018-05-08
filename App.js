68f498da7718c308b61259ef9579a34110706725
John Williams <john@redstubble.com>
2018-05-08 12:32:22 +1200

    Add navigator

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Tabs />
+        <Tabs />
       </View>
     );
   }
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
